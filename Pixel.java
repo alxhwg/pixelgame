e233121243410a87cf72e7412a8efef117fef73a
Alexander Hwang <a.hwang@berkeley.edu>
2015-08-03 21:15:22 -0700

    With the changes made to Terrain, Pixel now represents an inhabitant of the game grid, replacing the function that was Creature.java, which has now been removed.

--- Pixel.java
+++ Pixel.java
@@ -1,34 +1,38 @@
 package pixelgame;
+
 import java.awt.Color;
 
-/** The abstract class representing a pixel that inhabits the
-  * game grid.
-  */
+/** The abstract class representing an inhabitant of the
+ *  game grid.
+ */
 
 public abstract class Pixel {
 	private int xCoord;
 	private int yCoord;
-	private String name;
-	private int health;
-
-	/**Creates a Pixel object with name n and health h.*/
-	public Pixel(String n, int h) {
-		name = n;
-		health = h;
+	/** In the case the move order of two adjacent pixels needs to be determined;
+	 *  the higher the value, the higher the move priority.
+	 *  Rank can also impact certain Pixel's decisions regarding other Pixels. */
+	private int rank;
+	
+	private Color color;
+	
+	/** Returns the Pixel's x-coordinate. */
+	public int getX() {
+		return xCoord;
 	}
-
-	/**Returns the name.*/
-	public String name() {
-		return name;
-	}	
-
-	/**Returns the health.*/
-	public String health() {
-		return health;
+	
+	/** Returns the Pixel's y-coordinate. */
+	public int getY() {
+		return yCoord;
 	}
-
-	public void setHealth(int loss) {
-		health = health - loss;
+	
+	/** Returns the Pixel's rank. */
+	public int getRank() {
+		return rank;
 	}
-
+	
+	/** Returns the Pixel's color. */
+	public Color getColor() {
+		return color;
+	}
 }
